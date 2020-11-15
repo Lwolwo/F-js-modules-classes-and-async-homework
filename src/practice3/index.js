56589bb1f43f8e12f4f50befff32308d67e5cd33
Lwolwo <26643253+Lwolwo@users.noreply.github.com>
2020-11-15 20:10:29 +0800

    feat: finish pratice1、2、3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,8 @@
-export const getPoetry
+export const getPoetry = async () => {
+  // return fetch('https://v1.jinrishici.com/all.json')
+  //   .then((response) => response.json())
+  //   .then((data) => [data.origin, data.author, data.content]);
+  const data = await (await fetch('https://v1.jinrishici.com/all.json')).json();
+  const {origin, author, content} = data;
+  return [origin, author, content];
+}
